94c37aea56cf859f2e74e0763db3be4712251001
Ronnie <1017268540@qq.com>
2020-01-05 15:00:35 +0800

    图片中多个人脸的识别

--- find_who.py
+++ find_who.py
@@ -12,24 +12,27 @@ def find_person(photo):
     for index, face in enumerate(dets):
         shape = shape_predictor(img, face)
         face_descriptor = face_rec_model.compute_face_descriptor(img, shape)  # 计算人脸的128维的向量
-        for i in face_descriptor:
-            target.append(i)
+        target.append([i for i in face_descriptor])
     global data
-    temp = []
-    if target:
-        for name, vector in data:
-            if np.linalg.norm(np.array(target) - np.array(vector)) < 0.6:
-                temp.append([name, np.linalg.norm(np.array(target) - np.array(vector))])
+    result = []
+    if target:  # 识别出的人脸
+        for face in target:  # 对每个人脸进行比对
+            temp = []
+            for name, vector in data:
+                if np.linalg.norm(np.array(face) - np.array(vector)) < 0.6:
+                    temp.append([name, np.linalg.norm(np.array(face) - np.array(vector))])
+                else:
+                    continue
+            if temp:  # 在阈值0.6的条件下，找出相似度最高的选项
+                similar = ['', 0.6]
+                for i in temp:
+                    if i[1] < similar[1]:
+                        similar = i
+                result.append(similar[0])
             else:
-                continue
-        if temp:
-            similar = ['', 0.6]
-            for i in temp:
-                if i[1] < similar[1]:
-                    similar = i
-            cv2.imwrite('photos/find_who/result/' + similar[0] + photo, img)
-        else:
-            cv2.imwrite('photos/find_who/result/' + 'unpaired' + photo, img)
+                result.append('unpaired')
+        print(result)
+        cv2.imwrite('photos/find_who/result/' + '-'.join(result) + photo, img)
     else:
         cv2.imwrite('photos/find_who/result/' + 'unrecognized' + photo, img)
 
