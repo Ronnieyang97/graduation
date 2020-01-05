a7538249c8bc6412f507cfc7c4116a0041f3c2f0
Ronnie <1017268540@qq.com>
2020-01-05 12:14:31 +0800

    图片中单张人脸识别

--- find_who.py
+++ find_who.py
@@ -1,6 +1,37 @@
 import dlib
 import cv2
+import os
+import sqlite3
 import numpy as np
+
+
+def find_person(photo):
+    img = cv2.imread('photos/find_who/' + photo)
+    dets = detector(img, 1)
+    target = []
+    for index, face in enumerate(dets):
+        shape = shape_predictor(img, face)
+        face_descriptor = face_rec_model.compute_face_descriptor(img, shape)  # 计算人脸的128维的向量
+        for i in face_descriptor:
+            target.append(i)
+    global data
+    temp = []
+    if target:
+        for name, vector in data:
+            if np.linalg.norm(np.array(target) - np.array(vector)) < 0.6:
+                temp.append([name, np.linalg.norm(np.array(target) - np.array(vector))])
+            else:
+                continue
+        if temp:
+            similar = ['', 0.6]
+            for i in temp:
+                if i[1] < similar[1]:
+                    similar = i
+            cv2.imwrite('photos/find_who/result/' + similar[0] + photo, img)
+        else:
+            cv2.imwrite('photos/find_who/result/' + 'unpaired' + photo, img)
+    else:
+        cv2.imwrite('photos/find_who/result/' + 'unrecognized' + photo, img)
 
 
 # 模型及初始化
@@ -9,36 +40,17 @@ face_rec_model_path = "models/dlib_face_recognition_resnet_model_v1.dat"
 detector = dlib.get_frontal_face_detector()
 shape_predictor = dlib.shape_predictor(predictor_path)
 face_rec_model = dlib.face_recognition_model_v1(face_rec_model_path)
+files = [file for file in os.listdir('photos/find_who') if file.endswith('.jpg')]
+db = sqlite3.connect('face.db')
+cursor = db.cursor()
+database = cursor.execute('''select * from person''')
+data = []
+for info in database:
+    name = info[0]
+    vector = [float(num) for num in info[1].split('/') if num]
+    data.append([name, vector])
 
-test1 = cv2.imread('4.jpg')
-test2 = cv2.imread('5.jpg')
-test3 = cv2.imread('6.jpg')
-dets1 = detector(test1, 1)
-dets2 = detector(test2, 1)
-dets3 = detector(test3, 1)
-
-x = []
-for index, face in enumerate(dets1):
-    shape = shape_predictor(test1, face)
-    face_descriptor = face_rec_model.compute_face_descriptor(test1, shape)  # 计算人脸的128维的向量
-    x.append(face_descriptor)
-
-for index, face in enumerate(dets2):
-    shape = shape_predictor(test2, face)
-    for i, pt in enumerate(shape.parts()):
-        pt_pos = (pt.x, pt.y)
-    face_descriptor = face_rec_model.compute_face_descriptor(test2, shape)  # 计算人脸的128维的向量
-    x.append(face_descriptor)
-
-for index, face in enumerate(dets3):
-    shape = shape_predictor(test3, face)
-    for i, pt in enumerate(shape.parts()):
-        pt_pos = (pt.x, pt.y)
-    face_descriptor = face_rec_model.compute_face_descriptor(test3, shape)  # 计算人脸的128维的向量
-    x.append(face_descriptor)
-
-distance = 0
-for i in range(len(x[0])):
-    distance += (x[1][i] - x[2][i])**2
-print(np.sqrt(distance))
+for file in files:
+    find_person(file)
+    print(file + '  Done!')
 
