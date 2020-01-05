5e18ef3174afa24e4f14f9f863c200c6d34094b4
Ronnie <1017268540@qq.com>
2020-01-05 16:05:42 +0800

    将初始化模型的部分放入models.py

--- add_data.py
+++ add_data.py
@@ -1,7 +1,6 @@
 import os
-import sqlite3
-import dlib
 import cv2
+from models import *
 
 
 def get_data(photo):
@@ -14,20 +13,10 @@ def get_data(photo):
         face_descriptor = face_rec_model.compute_face_descriptor(img, shape)
         for i in face_descriptor:  # 将128维向量转为字符串保存
             temp += (str(i) + '/')
-    db = sqlite3.connect('face.db')
-    c = db.cursor()
-    c.execute('''insert into person (name, face_data)\
-    values(?, ?);''', [name, temp])
+    cursor.execute('''insert into person (name, face_data) values (?, ?);''', [name, temp])
     db.commit()
     db.close()
 
-
-# 模型及初始化
-predictor_path = "models/shape_predictor_68_face_landmarks.dat"
-face_rec_model_path = "models/dlib_face_recognition_resnet_model_v1.dat"
-detector = dlib.get_frontal_face_detector()
-shape_predictor = dlib.shape_predictor(predictor_path)
-face_rec_model = dlib.face_recognition_model_v1(face_rec_model_path)
 
 photos = [file for file in os.listdir('add') if file.endswith('.jpg')]
 for photo in photos:
