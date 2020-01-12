3516725cd8cdecb169646941ee501ed6894a9aba
Ronnie <1017268540@qq.com>
2020-01-12 19:37:33 +0800

    测试用图片

--- test.py
+++ test.py
@@ -1,16 +1,36 @@
-import pymongo
+from selenium import webdriver
+from selenium.webdriver.support.expected_conditions import visibility_of_element_located
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+import shutil
+import os
+import cv2
 
-test = pymongo.MongoClient("mongodb://localhost:27017/")
+'''web = webdriver.Chrome()
+web.get('http://iamge.baidu.com')
+target = '姜文'
+path = 'C://Users/Ronnie/PycharmProjects/graduation/test_data/'
+web.find_element_by_id('kw').send_keys(target)
+web.find_element_by_class_name('s_search').click()
 
-db = test['test_db']
+if not os.path.exists(path + target):
+    os.makedirs(path + target)
 
-collection = db['test_collection']
+for i in range(1, 3):
+    web.find_element_by_xpath('//*[@id="imgid"]/div/ul/li[' + str(i) + ']/div/a/img').click()
+    web.switch_to.window(web.window_handles[-1])
+    web.save_screenshot(str(i) + '.png')
+    #WebDriverWait(web, 10, 0.5).until(visibility_of_element_located((By.CLASS_NAME, 'down')))  # 显性等待
+    #web.find_element_by_class_name('down').click()
+    web.close()
+    web.switch_to.window(web.window_handles[-1])
 
-mydict = {"name": "John", "address": "Highway 37"}
+files = os.listdir()
+print(files)
+for file in files:
+    shutil.move('C://users/ronnie/downloads/' + file, path+target)'''
 
-mydict2 = [
-    {"name": "Amy", "address": "Apple st 652"},
-    {"name": "Hannah", "address": "Mountain 21"}]
 
-collection.insert(mydict2)
-print([x['address'] for x in collection.find()])
+
+img = cv2.imread('1.png')
+cv2.imwrite('new1.png', img[64:700, 0:900])
